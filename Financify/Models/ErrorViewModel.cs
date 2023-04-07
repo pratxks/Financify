99956a92fe95bd8013fdb5e8358d90798e08c073
pratxks <101081557+pratxks@users.noreply.github.com>
2023-04-07 05:32:17 +0530

    Add project files.

---
+++ Financify/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Financify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
