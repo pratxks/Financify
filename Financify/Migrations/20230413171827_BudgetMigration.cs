54ade70dc0b3d3ce25289a7e95307683063306e2
pratxks <101081557+pratxks@users.noreply.github.com>
2023-04-13 22:52:10 +0530

    Added Migrations for Budget and Transaction

---
+++ Financify/Migrations/20230413171827_BudgetMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Financify.Migrations
+{
+    public partial class BudgetMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(unicode: false, maxLength: 50, nullable: false),
+                    Income = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FoodBudget = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    HousingBudget = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    EntertainmentBudget = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OtherBudget = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Budgets");
+        }
+    }
+}
--- Financify/Startup.cs
+++ Financify/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NewFinancify.Data;
+using Financify.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
