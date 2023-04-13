819e49f31e89cc4321c8b320284b1f349f20a099
pratxks <101081557+pratxks@users.noreply.github.com>
2023-04-13 22:42:23 +0530

    Added TransactionViewModel to transfer transaction data between view & controller

---
+++ Financify/Models/TransactionViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Financify.Models
+{
+    public class TransactionViewModel
+    {
+        public string USERID { get; set; }
+
+        public string Category { get; set; }
+
+        public List<string> TransactionCategoryList { get; set; }
+
+        public List<Budget> BudgetList { get; set; }
+
+        public List<Transaction> TransactionList { get; set; }
+
+        public List<Transaction> FoodTransList { get; set; }
+
+        public List<Transaction> HousingTransList { get; set; }
+
+        public List<Transaction> EntertainmentTransList { get; set; }
+
+        public List<Transaction> OtherTransList { get; set; }
+    }
+}
